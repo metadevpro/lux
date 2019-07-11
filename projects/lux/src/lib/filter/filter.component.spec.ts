c9377ceffbc2c24509f1f4a5f5df7e548d843c2b
Pedro J. Molina <pjmolina@gmail.com>
2019-07-11 11:16:39 +0200

    timed-tests to filter component

--- projects/lux/src/lib/filter/filter.component.spec.ts
+++ projects/lux/src/lib/filter/filter.component.spec.ts
@@ -4,6 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { FilterComponent } from './filter.component';
 
 
+function inputKey(el: HTMLElement, keyPressed: string) {
+  const event = new KeyboardEvent('keyup', {
+      key: keyPressed
+  });
+  el.focus();
+  el.dispatchEvent(event);
+}
+
 describe('FilterComponent', () => {
   let component: FilterComponent;
   let fixture: ComponentFixture<FilterComponent>;
@@ -27,25 +35,117 @@ describe('FilterComponent', () => {
   });
 
   it('should show button when searchOntype is false', () => {
-    // Arrange
     component.searchOnType = false;
 
-    // Act
     fixture.detectChanges();
-    // Assert
     const button = fixture.nativeElement.querySelector('button');
     expect(button).toBeTruthy();
   });
 
   it('should not show button when searchOntype is true', () => {
-    // Arrange
     component.searchOnType = true;
 
-    // Act
     fixture.detectChanges();
-    // Assert
     const button = fixture.nativeElement.querySelector('button');
     expect(button).toBeFalsy();
   });
+
+  it('it should launch search after keypress + debounce time', async(() => {
+    // Arrange
+    component.searchOnType = true;
+    component.debounce = 520; // ms
+    fixture.detectChanges();
+
+    const input = fixture.nativeElement.querySelector('input');
+    const t0 = Date.now();
+    let t1 = null;
+
+    const sub = component.searchValueChange.subscribe(val => {
+      t1 = Date.now();
+
+      // Assert
+      const delay = t1 - t0;
+      expect(delay).toBeGreaterThanOrEqual(component.debounce);
+
+      sub.unsubscribe();
+    });
+
+    // Act
+    inputKey(input, 'A');
+    fixture.detectChanges();
+
+  }));
+  it('by default debounce time is 300 ms', async(() => {
+    // Arrange
+    component.searchOnType = true;
+    const input = fixture.nativeElement.querySelector('input');
+    const t0 = Date.now();
+    let t1 = null;
+
+    const sub = component.searchValueChange.subscribe(val => {
+      t1 = Date.now();
+
+      // Assert
+      const delay = t1 - t0;
+      expect(delay).toBeGreaterThanOrEqual(component.debounce);
+
+      sub.unsubscribe();
+    });
+
+    // Act
+    fixture.detectChanges();
+    inputKey(input, 'A');
+    fixture.detectChanges();
+
+  }));
+
+  it('enter should trigger search inmediatly', async(() => {
+    // Arrange
+    component.searchOnType = true;
+    const input = fixture.nativeElement.querySelector('input');
+    const t0 = Date.now();
+    let t1 = null;
+
+    const sub = component.searchValueChange.subscribe(val => {
+      t1 = Date.now();
+
+      // Assert
+      const delay = t1 - t0;
+      expect(delay).toBeLessThan(10);
+
+      sub.unsubscribe();
+    });
+
+    // Act
+    fixture.detectChanges();
+    component.keyup(new KeyboardEvent('keyup', { key: 'Enter' }));
+    fixture.detectChanges();
+
+  }));
+  it('clear() should trigger search inmediatly', async(() => {
+    // Arrange
+    component.searchOnType = true;
+    component.searchValue = 'ABC';
+    const input = fixture.nativeElement.querySelector('input');
+    const t0 = Date.now();
+    let t1 = null;
+
+    const sub = component.searchValueChange.subscribe(val => {
+      t1 = Date.now();
+
+      // Assert
+      const delay = t1 - t0;
+      expect(delay).toBeLessThan(10);
+
+      sub.unsubscribe();
+    });
+
+    // Act
+    fixture.detectChanges();
+    component.clear();
+    fixture.detectChanges();
+
+  }));
+
 
 });
