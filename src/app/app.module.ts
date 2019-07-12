34befc258fb247f5199866a5e3fe4f236d8ddbbc
hector23rp <hector23rp@gmail.com>
2019-07-12 10:17:52 +0200

    Feature/prism (#12)
    * Added prism js
    * Added Prim in checkbox & Deleted code class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { PaginationSampleComponent } from './pagination-sample/pagination-sample
 import { CoreModule } from './core/core.module';
 import { InputSampleComponent } from './input-sample/input-sample.component';
 import { CheckboxSampleComponent } from './checkbox-sample/checkbox-sample.component';
+import { PrismService } from './core/services/prism-service.service';
 
 
 const appRoutes: Routes = [
@@ -42,7 +43,9 @@ const appRoutes: Routes = [
     BrowserModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    PrismService
+  ],
   bootstrap: [ AppComponent]
 })
 export class AppModule { }
--- src/app/checkbox-sample/checkbox-sample.component.ts
+++ src/app/checkbox-sample/checkbox-sample.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentInit } from '@angular/core';
+import { PrismService } from '../core/services/prism-service.service';
 
 @Component({
   selector: 'app-checkbox-sample',
   templateUrl: './checkbox-sample.component.html'
 })
-export class CheckboxSampleComponent  {
+export class CheckboxSampleComponent implements AfterContentInit {
+
+  constructor(private prismService: PrismService) { }
+
+  ngAfterContentInit(): void {
+    this.prismService.highlightAll();
+  }
+
 }
