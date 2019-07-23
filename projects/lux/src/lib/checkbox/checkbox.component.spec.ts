ecd180747e267167f5e6bc3ec57b6f85113f5d79
Chanell Salas <csalas@metadev.pro>
2019-07-24 00:29:00 +0200

    add test tabindexValue and disabled

--- projects/lux/src/lib/checkbox/checkbox.component.spec.ts
+++ projects/lux/src/lib/checkbox/checkbox.component.spec.ts
@@ -9,10 +9,10 @@ describe('CheckboxComponent', () => {
   let fixture: ComponentFixture<CheckboxComponent>;
 
   beforeEach(async(() => {
-      TestBed.configureTestingModule({
-        imports: [ FormsModule ],
-        declarations: [ CheckboxComponent ]
-      })
+    TestBed.configureTestingModule({
+      imports: [FormsModule],
+      declarations: [CheckboxComponent]
+    })
       .compileComponents();
   }));
 
@@ -54,32 +54,32 @@ describe('CheckboxComponent', () => {
     expect(fixture.nativeElement.textContent).toContain('Yes');
     const slider = fixture.nativeElement.querySelector('.switch-item');
     expect(slider).toBeTruthy();
- });
+  });
 
   it('should render no slider when disabled', () => {
-     // Arrange
-     component.value = true;
-     component.disabled = true;
-     // Act
-     fixture.detectChanges();
-     // Assert
-     expect(fixture.nativeElement.textContent).toContain('Yes');
-     const slider = fixture.nativeElement.querySelector('.switch-item');
-     expect(slider).toBeNull();
+    // Arrange
+    component.value = true;
+    component.disabled = true;
+    // Act
+    fixture.detectChanges();
+    // Assert
+    expect(fixture.nativeElement.textContent).toContain('Yes');
+    const slider = fixture.nativeElement.querySelector('.switch-item');
+    expect(slider).toBeNull();
   });
 
   it('should render label if pressent', () => {
-      // Arrange
-      component.value = true;
-      component.label = 'Chanell';
-      // Act
-      fixture.detectChanges();
-      // Assert
-      const label = fixture.nativeElement.querySelector('label');
+    // Arrange
+    component.value = true;
+    component.label = 'Chanell';
+    // Act
+    fixture.detectChanges();
+    // Assert
+    const label = fixture.nativeElement.querySelector('label');
 
-      expect(label).toBeTruthy();
-      expect(label.textContent).toContain('Chanell');
-      expect(fixture.nativeElement.textContent).toContain('Yes');
+    expect(label).toBeTruthy();
+    expect(label.textContent).toContain('Chanell');
+    expect(fixture.nativeElement.textContent).toContain('Yes');
   });
 
   it('should render no label if no label property is present', () => {
@@ -143,7 +143,7 @@ describe('CheckboxComponent', () => {
     fixture.detectChanges();
     fixture.nativeElement.focus();
     fixture.detectChanges();
-    component.onKey({ key: ' ', preventDefault: () => {} } as KeyboardEvent);
+    component.onKey({ key: ' ', preventDefault: () => { } } as KeyboardEvent);
     fixture.detectChanges();
     // Assert
     expect(component.value).toBe(false);
@@ -157,7 +157,7 @@ describe('CheckboxComponent', () => {
     fixture.detectChanges();
     fixture.nativeElement.focus();
     fixture.detectChanges();
-    component.onKey({ key: ' ', preventDefault: () => {} } as KeyboardEvent);
+    component.onKey({ key: ' ', preventDefault: () => { } } as KeyboardEvent);
     fixture.detectChanges();
 
     // Assert
@@ -172,7 +172,7 @@ describe('CheckboxComponent', () => {
     fixture.detectChanges();
     fixture.nativeElement.focus();
     fixture.detectChanges();
-    component.onKey({ key: ' ', preventDefault: () => {} } as KeyboardEvent);
+    component.onKey({ key: ' ', preventDefault: () => { } } as KeyboardEvent);
     fixture.detectChanges();
 
     // Assert
@@ -186,10 +186,28 @@ describe('CheckboxComponent', () => {
     fixture.detectChanges();
     fixture.nativeElement.focus();
     fixture.detectChanges();
-    component.onKey({ key: ' ', preventDefault: () => {} } as KeyboardEvent);
+    component.onKey({ key: ' ', preventDefault: () => { } } as KeyboardEvent);
     fixture.detectChanges();
 
     // Assert
     expect(component.value).toBe(true);
+  });
+
+  it('should tabindexValue null when disabled is true', () => {
+    // Arrange
+    component.disabled = true;
+    // Act
+    fixture.detectChanges();
+    // Assert
+    expect(component.tabindexValue).toBe(null);
+  });
+
+  it('should tabindexValue 0 when disabled is false', () => {
+    // Arrange
+    component.disabled = false;
+    // Act
+    fixture.detectChanges();
+    // Assert
+    expect(component.tabindexValue).toBe('0');
   });
 });
