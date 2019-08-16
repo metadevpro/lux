8d1ccb95268748626f3d3d3244d769b522287446
hector23rp <hector23rp@gmail.com>
2019-08-16 12:43:56 +0200

    Init test Tooltip

--- projects/lux/src/lib/directives/tooltip.directive.spec.ts
+++ projects/lux/src/lib/directives/tooltip.directive.spec.ts
@@ -1,1 +1,34 @@
-// TO DO
+import { async, TestBed, ComponentFixture } from '@angular/core/testing';
+import { Component, DebugElement } from '@angular/core';
+
+import { LuxTooltipDirective } from './tooltip.directive';
+
+@Component({selector: 'lux-test-cmp', template: ''})
+export class TestComponent {
+}
+
+export function createTestComponent(html: string): ComponentFixture<TestComponent> {
+    TestBed.overrideComponent(TestComponent, {set: {template: html}});
+    const fixture = TestBed.createComponent(TestComponent);
+    fixture.detectChanges();
+    return fixture as ComponentFixture<TestComponent>;
+}
+
+fdescribe('TooltipDirective', () => {
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ LuxTooltipDirective, TestComponent ]
+          })
+          .compileComponents();
+    }));
+
+    it('it should be created component with tooltip declarative', () => {
+        const fixture = createTestComponent('<button luxTooltip>Button with Tooltip</button>');
+        expect(fixture.componentInstance).toBeTruthy();
+    });
+
+    it('it should display default tooltip', () => {
+    });
+
+});
