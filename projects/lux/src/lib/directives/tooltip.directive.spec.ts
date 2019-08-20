05ba167944bdc1d1c01b4f9b4ef0e231aae210a0
hector23rp <hector23rp@gmail.com>
2019-08-20 09:07:17 +0200

    Added test

--- projects/lux/src/lib/directives/tooltip.directive.spec.ts
+++ projects/lux/src/lib/directives/tooltip.directive.spec.ts
@@ -1,34 +1,58 @@
-import { async, TestBed, ComponentFixture } from '@angular/core/testing';
-import { Component, DebugElement } from '@angular/core';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { Component, DebugElement, NgModule } from '@angular/core';
 
 import { LuxTooltipDirective } from './tooltip.directive';
+import { TooltipService } from './tooltip.service';
+import { By } from '@angular/platform-browser';
+import { TooltipComponent } from './tooltip.component';
 
-@Component({selector: 'lux-test-cmp', template: ''})
+@Component({selector: 'lux-test-cmp', template: '<p>Hello</p>'})
 export class TestComponent {
 }
 
 export function createTestComponent(html: string): ComponentFixture<TestComponent> {
-    TestBed.overrideComponent(TestComponent, {set: {template: html}});
-    const fixture = TestBed.createComponent(TestComponent);
+    const fixture = TestBed.overrideComponent(TestComponent, {
+        set: {
+            template: html
+        }}).createComponent(TestComponent);
     fixture.detectChanges();
     return fixture as ComponentFixture<TestComponent>;
 }
 
+@NgModule({
+    declarations: [ LuxTooltipDirective, TestComponent, TooltipComponent ],
+    entryComponents: [TooltipComponent],
+    providers: [ TooltipService ]
+})
+class TooltipTestModule {}
+
 fdescribe('TooltipDirective', () => {
 
-    beforeEach(async(() => {
+    beforeEach(() => {
         TestBed.configureTestingModule({
-            declarations: [ LuxTooltipDirective, TestComponent ]
+            imports: [TooltipTestModule]
           })
           .compileComponents();
-    }));
+    });
 
     it('it should be created component with tooltip declarative', () => {
         const fixture = createTestComponent('<button luxTooltip>Button with Tooltip</button>');
         expect(fixture.componentInstance).toBeTruthy();
     });
 
-    it('it should display default tooltip', () => {
+    it('it should display default tooltip', async () => {
+        const fixture = createTestComponent('<button luxTooltip>Button with Tooltip</button>');
+        // const button: DebugElement = fixture.debugElement.nativeElement.querySelector('button');
+        const directive: DebugElement = fixture.debugElement.query(By.directive(LuxTooltipDirective));
+        // button.dispatchEvent(new Event('mouseenter'));
+        /* button.triggerEventHandler('mouseenter', (res) => {
+            console.log(res);
+        }); */
+        const evt = document.createEvent('Event');
+        evt.initEvent('mouseenter', true, false);
+        directive.nativeElement.dispatchEvent(evt);
+        fixture.detectChanges();
+        console.log(fixture);
     });
 
 });
