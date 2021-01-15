3d14d25ad9bb955ca0b38bacacec1b5d4583da3c
Pedro J. Molina <pjmolina@gmail.com>
2021-01-15 18:24:45 +0100

    Autocomplete working implementation

--- projects/lux/src/lib/autocomplete/autocomplete.component.ts
+++ projects/lux/src/lib/autocomplete/autocomplete.component.ts
@@ -1,4 +1,14 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 export interface DataSourceItem<K, L> {
   key: K;
@@ -6,20 +16,23 @@ export interface DataSourceItem<K, L> {
 }
 export type DataSource<K, L> = DataSourceItem<K, L>[];
 
-const MAX_ITEMS_TO_SUGGEST = 10;
-
 @Component({
   selector: 'lux-autocomplete',
   templateUrl: './autocomplete.component.html',
   styleUrls: ['./autocomplete.component.scss'],
 })
-export class AutocompleteComponent {
+export class AutocompleteComponent implements OnInit, AfterViewInit {
+  @ViewChild('i0', { static: true }) i0: ElementRef;
+  @ViewChild('completeDiv', { static: true }) completeDiv: ElementRef;
+
   private _dataSource: DataSource<any, string>;
   private _required: boolean;
   private _placeholder: string;
   private _value: any;
 
   completionList: DataSource<any, string> = [];
+  showCompletion = false;
+  focusItem: DataSourceItem<any, string>;
 
   @Output() valueChange = new EventEmitter<any>();
   @Output() dataSourceChange = new EventEmitter<DataSource<any, string>>();
@@ -30,24 +43,20 @@ export class AutocompleteComponent {
   @Input() label = '';
 
   @Input()
-  get value(): any
-  {
+  get value(): any {
     return this._value;
   }
-  set value(v: any)
-  {
+  set value(v: any) {
     this._value = v;
     this.valueChange.emit(v);
-    const found = (this.dataSource || []).find(i => i.key === v);
+    const found = (this.dataSource || []).find((i) => i.key === v);
     this.label = found ? found.label : '';
   }
   @Input()
-  get dataSource(): DataSource<any, string>
-  {
+  get dataSource(): DataSource<any, string> {
     return this._dataSource;
   }
-  set dataSource(v: DataSource<any, string>)
-  {
+  set dataSource(v: DataSource<any, string>) {
     this._dataSource = v;
     this.dataSourceChange.emit(v);
   }
@@ -66,26 +75,141 @@ export class AutocompleteComponent {
     return this._placeholder ? this._placeholder : '';
   }
 
-  onKeypress(event: KeyboardEvent, label: string) {
+  constructor(
+    private cd: ChangeDetectorRef,
+  ) {}
+
+  ngOnInit(): void {
+    const found = this.dataSource.find((it) => it.key === this.value);
+    this.label = found ? found.label : '';
   }
-  onKeyup(event: KeyboardEvent, label: string) {
-    this.showCompletionList(label);
+  ngAfterViewInit(): void {
+    const width = this.i0.nativeElement.getBoundingClientRect().width;
+    this.completeDiv.nativeElement.style.width = `${width}px`;
   }
-  onLostFocus(label: string): void {
-    const found = (this.dataSource || []).find(it => it.label === label);
-    if (found && found.key !== this.value) {
-      this.value = found.key;
+
+  onKeydown(event: KeyboardEvent, label: string) {
+    switch (event.key) {
+      case 'Tab':
+        this.pickFirstMatch(label);
+        break;
     }
   }
-  showCompletionList(text: string): void {
-    const substring = (text || '').toLowerCase();
-    this.completionList = (this.dataSource || [])
-      .filter(it => it.label.toLowerCase().includes(substring))
-      .sort((a, b) => (a.label.localeCompare(b.label)))
-      .slice(0, MAX_ITEMS_TO_SUGGEST);
+  onKeypress(event: KeyboardEvent, label: string) {
+    switch (event.key) {
+      case 'Intro':
+      case 'Enter':
+        this.pickFirstMatch(label);
+        event.preventDefault();
+        break;
+    }
+  }
+  onKeyup(event: KeyboardEvent, label: string) {
+    switch (event.key) {
+      case 'PageDown':
+        this.focusOnNext(5);
+        event.preventDefault();
+        break;
+      case 'ArrowDown':
+        this.focusOnNext(1);
+        event.preventDefault();
+        break;
+      case 'PageUp':
+        this.focusOnPrevious(5);
+        event.preventDefault();
+        break;
+      case 'ArrowUp':
+        this.focusOnPrevious(1);
+        event.preventDefault();
+        break;
+      case 'Escape':
+        this.complete(null);
+        event.preventDefault();
+        break;
+      default:
+        this.showCompletionList(label);
+      // event.preventDefault();
+    }
+  }
+  private focusOnNext(offset: number) {
+    const list = this.completionList || [];
+    const index = list.findIndex(
+      (it) => this.focusItem && it.key === this.focusItem.key
+    );
+    const next =
+      index !== -1 && list.length > index + offset
+        ? list[index + offset]
+        : list[list.length - 1];
+    this.focusItem = next;
+    this.ensureItemVisible(index);
+  }
+  private focusOnPrevious(offset: number) {
+    const list = this.completionList || [];
+    const index = list.findIndex(
+      (it) => this.focusItem && it.key === this.focusItem.key
+    );
+    const next =
+      index !== -1 && index > offset ? list[index - offset] : list[0];
+    this.focusItem = next;
+    this.ensureItemVisible(index);
+  }
+  onLostFocus(label: string): void {
+    setTimeout(() => {
+      this.toogleCompletion(false, null);
+    }, 200);
   }
   complete(item: DataSourceItem<object, string>): void {
-    this.value = item.key;
-    this.label = item.label;
+    if (item !== null) {
+      this.value = item.key;
+      this.label = item.label;
+    } else {
+      this.value = null;
+      this.label = '';
+    }
+    this.showCompletion = false;
+    this.cd.markForCheck();
+  }
+  toogleCompletion(show: boolean, label: string): void {
+    if (show) {
+      this.showCompletionList(label);
+    } else {
+      this.showCompletion = false;
+    }
+    this.cd.markForCheck();
+  }
+
+  private ensureItemVisible(index: number): void {
+    const target = this.completeDiv.nativeElement.querySelectorAll('li')[index];
+    if (target) {
+      target.scrollIntoView({ block: 'center' });
+    }
+  }
+  private pickFirstMatch(text: string): void {
+    const source = (
+      (this.focusItem && this.focusItem.label) ||
+      text ||
+      ''
+    ).trim();
+    if (source === '') {
+      this.showCompletion = false;
+      return;
+    }
+    this.completionList = [];
+    const suggestions = this.computeCompletionList(source);
+    this.complete(
+      suggestions && suggestions.length > 0 ? suggestions[0] : null
+    );
+  }
+  private showCompletionList(text: string): void {
+    this.completionList = this.computeCompletionList(text);
+    this.focusItem =
+      this.completionList.length > 0 ? this.completionList[0] : null;
+    this.showCompletion = true;
+  }
+  private computeCompletionList(text: string): DataSource<any, string> {
+    const substring = (text || '').toLowerCase();
+    return (this.dataSource || [])
+      .filter((it) => it.label.toLowerCase().includes(substring))
+      .sort((a, b) => a.label.localeCompare(b.label));
   }
 }
