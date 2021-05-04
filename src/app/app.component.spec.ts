3e2c2e0ec13abfca305086145b7cd80efa0db0bf
Jesus Rodriguez <foxandxss@gmail.com>
2021-05-04 13:23:25 +0200

    remove test warnings

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed, waitForAsync } from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
 import { createComponentFactory } from '@ngneat/spectator';
@@ -5,7 +6,10 @@ import { createComponentFactory } from '@ngneat/spectator';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  const createComponent = createComponentFactory(AppComponent);
+  const createComponent = createComponentFactory({
+    component: AppComponent,
+    schemas: [NO_ERRORS_SCHEMA]
+  });
 
   it('should create the app', () => {
     const spectator = createComponent();
