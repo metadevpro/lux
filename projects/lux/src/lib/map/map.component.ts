b8f93470f908028959445bb4c9bebde7311ecc1d
Javier Centeno Vega <jacenve@telefonica.net>
2021-07-08 10:42:05 +0200

    Add readonly property to map

--- projects/lux/src/lib/map/map.component.ts
+++ projects/lux/src/lib/map/map.component.ts
@@ -47,6 +47,32 @@ export class MapComponent implements OnInit {
     return this._center;
   }
 
+  _readonly: boolean;
+  @Input()
+  set readonly(readonly: boolean) {
+    if (!readonly) {
+      if (this._map) {
+        this._map.on('click', (args: any) => {
+          // args.coordinate is in EPSG:3857 (meters), we transform it into EPSG:4326 (degrees)
+          const coordinates = ol.proj.toLonLat(args.coordinate);
+          // alternatively: const lonLat = ol.proj.transform(args.coordinate,'EPSG:3857','EPSG:4326');
+          this.addMarkerAtCoordinates(coordinates);
+        });
+      }
+    } else {
+      if (this._map) {
+        this._map.on('click', (args: any) => {});
+      }
+    }
+    this._readonly = readonly;
+    // reset marker coordinates so marker interactions can be created or recreated accordingly
+    // to the changes to readonly
+    this.markerCoordinates = this.markerCoordinates;
+  }
+  get readonly(): boolean {
+    return this._readonly;
+  }
+
   private _marker: any;
   private _markerCoordinates: number[];
   @Input()
@@ -106,13 +132,6 @@ export class MapComponent implements OnInit {
       })
     });
 
-    this._map.on('click', (args: any) => {
-      // args.coordinate is in EPSG:3857 (meters), we transform it into EPSG:4326 (degrees)
-      const coordinates = ol.proj.toLonLat(args.coordinate);
-      // alternatively: const lonLat = ol.proj.transform(args.coordinate,'EPSG:3857','EPSG:4326');
-      this.addMarkerAtCoordinates(coordinates);
-    });
-
     // TODO
     // this._map.on('resize', () => {
     //   this._map.redraw();
@@ -156,6 +175,15 @@ export class MapComponent implements OnInit {
       this.zoom = this.zoom;
     }
 
+    if (this.readonly === undefined || this.readonly === null) {
+      // if readonly is not set, we set its default value
+      this.readonly = false;
+    } else {
+      // readonly needs the map to be properly set, so if it was set before the map was initialized,
+      // we set readonly again to perform necessary adjustements
+      this.readonly = this.readonly;
+    }
+
     if (
       this.markerCoordinates !== undefined &&
       this.markerCoordinates !== null
@@ -180,11 +208,14 @@ export class MapComponent implements OnInit {
       this._markerSource.addFeature(this._marker);
       this._markerCoordinates = this.getMarkerCoordinates();
 
-      const dragInteraction = new ol.interaction.Modify({
-        features: new ol.Collection([this._marker]),
-        style: MapComponent._markerStyle
-      });
-      this._map.addInteraction(dragInteraction);
+      if (!this.readonly) {
+        const dragInteraction = new ol.interaction.Modify({
+          features: new ol.Collection([this._marker]),
+          style: MapComponent._markerStyle
+        });
+        this._map.addInteraction(dragInteraction);
+      }
+
       this._marker.on(
         'change',
         () => {
