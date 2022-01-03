c37254ae919531bdd7b6b23fde33ebf41b694476
Javier Centeno Vega <jacenve@telefonica.net>
2022-01-03 12:19:35 +0100

    Implement url type in input component (#171)
    * Implement url type in input component
    * Allow any arbitrary protocol in url validity check
    * Display link button only when input has value in type=URL
    * Update error message to include protocol

--- projects/lux/src/lib/input/input.component.ts
+++ projects/lux/src/lib/input/input.component.ts
@@ -21,6 +21,7 @@ import {
   isInitialAndEmpty,
   isValidEmail,
   isValidNumber,
+  isValidUrl,
   normalizeDate
 } from '../helperFns';
 import { languageDetector } from '../lang';
@@ -66,13 +67,15 @@ export class InputComponent implements OnInit, ControlValueAccessor, Validator {
       required: 'Required field.',
       min: 'Minimum value is $min.',
       max: 'Maximum value is $max.',
-      email: 'Format should match example@example.com.'
+      email: 'Format should match example@example.com.',
+      url: 'Format should match https://example.com.'
     },
     es: {
       required: 'El campo es obligatorio.',
       min: 'El valor mínimo es $min.',
       max: 'El valor máximo es $max.',
-      email: 'El campo debe tener un formato como ejemplo@ejemplo.com.'
+      email: 'El campo debe tener un formato como ejemplo@ejemplo.com.',
+      url: 'El campo debe tener un formato como https://ejemplo.com.'
     }
   };
 
@@ -253,6 +256,10 @@ export class InputComponent implements OnInit, ControlValueAccessor, Validator {
       result = result || {};
       result.email = { value, reason: 'Invalid email.' };
     }
+    if (this.type === 'url' && hasValue(value) && !isValidUrl(value)) {
+      result = result || {};
+      result.url = { value, reason: 'Invalid URL.' };
+    }
     if (this.type === 'number' && hasValue(value) && !isValidNumber(value)) {
       result = result || {};
       result.numeric = { value, reason: 'Invalid number.' };
@@ -335,6 +342,9 @@ export class InputComponent implements OnInit, ControlValueAccessor, Validator {
     this.keyPress.emit(event);
   }
 
+  isUrl(): boolean {
+    return this.type === 'url';
+  }
   isNumber(): boolean {
     return this.type === 'number';
   }
@@ -349,7 +359,10 @@ export class InputComponent implements OnInit, ControlValueAccessor, Validator {
   }
   hasPostfix(): boolean {
     return (
-      this.currency === 'EUR' || this.isPercentage() || this.isPermillage()
+      this.currency === 'EUR' ||
+      this.isPercentage() ||
+      this.isPermillage() ||
+      (this.isUrl() && !!this.value)
     );
   }
 
@@ -367,6 +380,9 @@ export class InputComponent implements OnInit, ControlValueAccessor, Validator {
     switch (type) {
       case 'email':
         this.setEmailPatterns();
+        break;
+      case 'url':
+        this.setUrlPatterns();
         break;
       case 'date':
         this.setDatePatterns();
@@ -395,6 +411,8 @@ export class InputComponent implements OnInit, ControlValueAccessor, Validator {
   }
 
   setEmailPatterns(): void {}
+
+  setUrlPatterns(): void {}
 
   setDatePatterns(): void {
     this.min = this.min || '1900-01-01';
