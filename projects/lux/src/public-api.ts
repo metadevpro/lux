91d16c3ee31f04fa617dbcfa978bd934b1337a7a
Jesus Rodriguez <foxandxss@gmail.com>
2022-01-11 17:06:53 +0100

    update to Angular 13

--- projects/lux/src/public-api.ts
+++ projects/lux/src/public-api.ts
@@ -2,27 +2,32 @@
  * Public API Surface of lux
  */
 
-export { LuxModule } from './lib/lux.module';
+export { AutocompleteListComponent } from './lib/autocomplete-list/autocomplete-list.component';
+export { AutocompleteComponent } from './lib/autocomplete/autocomplete.component';
+export {
+  BreadcrumbItem,
+  LuxBreadcrumbComponent
+} from './lib/breadcrumb/breadcrumb.component';
+export { CheckboxComponent } from './lib/checkbox/checkbox.component';
 export {
   DataSource,
   DataSourceItem,
   DecoratedDataSource,
   DecoratedDataSourceItem
 } from './lib/datasource';
-export { AutocompleteComponent } from './lib/autocomplete/autocomplete.component';
-export { AutocompleteListComponent } from './lib/autocomplete-list/autocomplete-list.component';
-export {
-  LuxBreadcrumbComponent,
-  BreadcrumbItem
-} from './lib/breadcrumb/breadcrumb.component';
-export { CheckboxComponent } from './lib/checkbox/checkbox.component';
+export { DatetimeComponent } from './lib/datetime/datetime.component';
 export { FilterComponent } from './lib/filter/filter.component';
+export { GeolocationComponent } from './lib/geolocation/geolocation.component';
 export { InputComponent } from './lib/input/input.component';
-export { PaginationComponent } from './lib/pagination/pagination.component';
-export { LuxTooltipDirective } from './lib/tooltip/tooltip.directive';
-export { ModalService } from './lib/modal/modal.service';
-export {
-  RadioItem,
-  RadiogroupComponent
-} from './lib/radiogroup/radiogroup.component';
+export { LuxModule } from './lib/lux.module';
+export { MapComponent } from './lib/map/map.component';
 export { ModalRef } from './lib/modal/modal-ref';
+export { ModalService } from './lib/modal/modal.service';
+export { PaginationComponent } from './lib/pagination/pagination.component';
+export {
+  RadiogroupComponent,
+  RadioItem
+} from './lib/radiogroup/radiogroup.component';
+export { SelectComponent } from './lib/select/select.component';
+export { LuxTooltipDirective } from './lib/tooltip/tooltip.directive';
+export { VoiceRecognitionDirective } from './lib/voice-recognition/voice-recognition.directive';
