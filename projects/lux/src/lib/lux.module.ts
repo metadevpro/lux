771a1cd7db15a4957a52e724b1838e7793a6f8e1
Pedro J. Molina <pjmolina@gmail.com>
2022-01-13 10:53:40 +0100

    upgrade angular-resize-3.1.1 renamed module to: AngularResizeEventModule

--- projects/lux/src/lib/lux.module.ts
+++ projects/lux/src/lib/lux.module.ts
@@ -1,27 +1,26 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
-import { AngularResizedEventModule } from 'angular-resize-event';
-import { AutocompleteComponent } from './autocomplete/autocomplete.component';
+import { AngularResizeEventModule } from 'angular-resize-event';
 import { AutocompleteListComponent } from './autocomplete-list/autocomplete-list.component';
+import { AutocompleteComponent } from './autocomplete/autocomplete.component';
+import { LuxBreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 import { CheckboxComponent } from './checkbox/checkbox.component';
 import { DatetimeComponent } from './datetime/datetime.component';
-import { InputComponent } from './input/input.component';
 import { FilterComponent } from './filter/filter.component';
 import { GeolocationComponent } from './geolocation/geolocation.component';
-import { HttpClientModule } from '@angular/common/http';
-import { LuxTooltipDirective } from './tooltip/tooltip.directive';
-import { LuxBreadcrumbComponent } from './breadcrumb/breadcrumb.component';
-import { LuxModalWindowComponent } from './modal/modal-window';
-import { LuxModalBackdropComponent } from './modal/modal-backdrop';
+import { InputComponent } from './input/input.component';
 import { MapComponent } from './map/map.component';
+import { LuxModalBackdropComponent } from './modal/modal-backdrop';
+import { LuxModalWindowComponent } from './modal/modal-window';
 import { ModalService } from './modal/modal.service';
 import { PaginationComponent } from './pagination/pagination.component';
 import { RadiogroupComponent } from './radiogroup/radiogroup.component';
 import { SelectComponent } from './select/select.component';
 import { TooltipComponent } from './tooltip/tooltip.component';
+import { LuxTooltipDirective } from './tooltip/tooltip.directive';
 import { TooltipService } from './tooltip/tooltip.service';
 import { VoiceRecognitionDirective } from './voice-recognition/voice-recognition.directive';
 import { WINDOW_PROVIDERS } from './window/window.service';
@@ -53,7 +52,7 @@ import { WINDOW_PROVIDERS } from './window/window.service';
   ],
   providers: [ModalService, TooltipService, WINDOW_PROVIDERS],
   imports: [
-    AngularResizedEventModule,
+    AngularResizeEventModule,
     FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
