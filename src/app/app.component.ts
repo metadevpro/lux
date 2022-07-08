41a160de86ac860c9de3af6f1d2088a45ffb7fc7
Pedro J. Molina <pjmolina@gmail.com>
2022-07-08 13:41:51 +0200

    add version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,14 @@
-import { Component, AfterContentInit } from '@angular/core';
-import { of } from 'rxjs';
-
+import { Component } from '@angular/core';
+import { VERSION } from 'src/environments/version';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
-
+export class AppComponent {
+  version = VERSION;
   title = 'lux-demo';
 
-  constructor() { }
-
-
-
+  constructor() {}
 }
